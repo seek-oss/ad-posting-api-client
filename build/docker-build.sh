2dbc4c3cfd79b4834c05e9aaec7aac4ab50bf658
Amy Lin <alin@seek.com.au>
2015-08-14 09:50:47 +1000

    remove the build files that are not used yet

--- build/docker-build.sh
+++
@@ -1,7 +0,0 @@
-docker run \
-    --rm \
-    -v $(which docker):/usr/local/bin/docker \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v $(pwd):/var/app \
-    -e "SEEK_TEAMCITY_VCSROOT_URL=$SEEK_TEAMCITY_VCSROOT_URL" -e "SEEK_TEAMCITY_VCSROOT_BRANCH=$SEEK_TEAMCITY_VCSROOT_BRANCH" -e "BUILD_VCS_NUMBER=$BUILD_VCS_NUMBER" -e "TEAMCITY_VERSION=$TEAMCITY_VERSION" -e "BUILD_NUMBER=$BUILD_NUMBER" \
-    dockerregistry.seekinfra.com/advertiser/adpostingapi_buildcontainer /var/app/build/build.sh "$@"
