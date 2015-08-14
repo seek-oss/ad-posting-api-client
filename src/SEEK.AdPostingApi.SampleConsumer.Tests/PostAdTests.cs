d5c4122f28959471985f4fe53d30a6fa4455ac3d
Grant Pedersen <gpedersen@seek.com.au>
2015-08-14 23:50:20 +1000

    update media type of index pact test

--- src/SEEK.AdPostingApi.SampleConsumer.Tests/PostAdTests.cs
+++ src/SEEK.AdPostingApi.SampleConsumer.Tests/PostAdTests.cs
@@ -57,7 +57,7 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
                     Status = 200,
                     Headers = new Dictionary<string, string>
                     {
-                        {"Content-Type", "application/json; charset=utf-8"}
+                        {"Content-Type", "application/hal+json; charset=utf-8"}
                     },
                     Body = new
                     {
