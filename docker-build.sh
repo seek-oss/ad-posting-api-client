275824e53f7f9ff39dc250223cd6fbb1a6cf9fa2
Martin Booth <mbooth@seek.com.au>
2015-08-17 11:20:34 +1000

    Added in docker build

---
+++ docker-build.sh
@@ -0,0 +1,7 @@
+docker run \
+    --rm \
+    -v $(which docker):/usr/local/bin/docker \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $(pwd):/var/app \
+    -e "SEEK_TEAMCITY_VCSROOT_URL=$SEEK_TEAMCITY_VCSROOT_URL" -e "SEEK_TEAMCITY_VCSROOT_BRANCH=$SEEK_TEAMCITY_VCSROOT_BRANCH" -e "BUILD_VCS_NUMBER=$BUILD_VCS_NUMBER" -e "TEAMCITY_VERSION=$TEAMCITY_VERSION" -e "BUILD_NUMBER=$BUILD_NUMBER" \
+    dockerregistry.seekinfra.com/aspnet:1.0.0-beta6 /var/app/build/build.sh "$@"
