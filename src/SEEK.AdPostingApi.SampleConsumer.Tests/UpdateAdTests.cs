b2131f7cdfa818ac587d451c563fb602703a65ee
Amy Lin <alin@seek.com.au>
2015-08-28 13:09:28 +1000

    added 204 case for pact test

--- src/SEEK.AdPostingApi.SampleConsumer.Tests/UpdateAdTests.cs
+++ src/SEEK.AdPostingApi.SampleConsumer.Tests/UpdateAdTests.cs
@@ -40,6 +40,95 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
         [Test]
         public async Task UpdateExistingAdvertisement()
         {
+            const string advertisementId = "8e2fde50-bc5f-4a12-9cfb-812e50500184";
+            OAuth2Token oAuth2Token = new OAuth2TokenBuilder().Build();
+
+            PactProvider.MockService
+                .Given(string.Format("There is an advertisement with id: '{0}'", advertisementId))
+                .UponReceiving("Update request for advertisement")
+                .With(new ProviderServiceRequest
+                {
+                    Method = HttpVerb.Put,
+                    Path = "/advertisement/" + advertisementId,
+                    Headers = new Dictionary<string, string>
+                    {
+                        {"Authorization", "Bearer " + oAuth2Token.AccessToken},
+                        {"Content-Type", "application/vnd.seek.advertisement+json; charset=utf-8"}
+                    },
+                    Body = new
+                    {
+                        agentId = (object)null,
+                        advertiserId = "9012",
+                        advertisementType = AdvertisementType.StandOut.ToString(),
+                        jobTitle = "Exciting Senior Developer role in a great CBD location. Great $$$ - updated",
+                        locationId = "378",
+                        subclassificationId = "734",
+                        workType = WorkType.FullTime.ToString(),
+                        salaryType = SalaryType.AnnualPackage.ToString(),
+                        salaryMinimum = 100000,
+                        salaryMaximum = 200000,
+                        salaryDetails = "We will pay you",
+                        jobSummary = "Developer job",
+                        advertisementDetails = "Exciting, do I need to say more?",
+                        contactDetails = "Call me",
+                        videoUrl = "https://www.youtube.com/watch?v=dVDk7PXNXB8",
+                        videoPosition = VideoPosition.Above.ToString(),
+                        applicationEmail = "asdf@asdf.com",
+                        applicationFormUrl = "http://applicationform/",
+                        screenId = 20,
+                        jobReference = "JOB1234",
+                        templateId = 99,
+                        templateItems = new[]
+                        {
+                            new { name = "Template Line 1", value = "Template Value 1" },
+                            new { name = "Template Line 2", value = "Template Value 2" }
+                        },
+                        standoutLogoId = 333,
+                        standoutBullet1 = "new Uzi",
+                        standoutBullet2 = "new Remington Model",
+                        standoutBullet3 = "new AK-47",
+                        additionalProperties = new[] { AdditionalPropertyType.ResidentsOnly.ToString() }
+                    }
+                })
+                .WillRespondWith(new ProviderServiceResponse { Status = 204 });
+
+            var client = new AdPostingApiClient(PactProvider.MockServiceUri, _oauthClient);
+
+            await client.UpdateAdvertisementAsync(new Uri(PactProvider.MockServiceUri, "advertisement/" + advertisementId), 
+                new Advertisement
+                    {
+                        AdvertiserId = "9012",
+                        AdvertisementType = AdvertisementType.StandOut,
+                        JobTitle = "Exciting Senior Developer role in a great CBD location. Great $$$ - updated",
+                        LocationId = "378",
+                        SubclassificationId = "734",
+                        WorkType = WorkType.FullTime,
+                        SalaryType = SalaryType.AnnualPackage,
+                        SalaryMinimum = 100000,
+                        SalaryMaximum = 200000,
+                        SalaryDetails = "We will pay you",
+                        JobSummary = "Developer job",
+                        AdvertisementDetails = "Exciting, do I need to say more?",
+                        ContactDetails = "Call me",
+                        VideoUrl = "https://www.youtube.com/watch?v=dVDk7PXNXB8",
+                        VideoPosition = VideoPosition.Above,
+                        ApplicationEmail = "asdf@asdf.com",
+                        ApplicationFormUrl = "http://applicationform/",
+                        ScreenId = 20,
+                        JobReference = "JOB1234",
+                        TemplateId = 99,
+                        TemplateItems = new[]
+                        {
+                            new TemplateItemModel{Name = "Template Line 1", Value = "Template Value 1"},
+                            new TemplateItemModel{Name = "Template Line 2", Value = "Template Value 2"}
+                        },
+                        StandoutLogoId = 333,
+                        StandoutBullet1 = "new Uzi",
+                        StandoutBullet2 = "new Remington Model",
+                        StandoutBullet3 = "new AK-47",
+                        AdditionalProperties = new[] { AdditionalPropertyType.ResidentsOnly }
+                });
+
         }
 
         [Test]
