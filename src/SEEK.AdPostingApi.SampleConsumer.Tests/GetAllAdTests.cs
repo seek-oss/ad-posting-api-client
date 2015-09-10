a0c277f5b1b8a516e6dea38169ade821923eece9
Conrad Lang <clang@seek.com.au>
2015-09-10 15:56:44 +1000

    Updated advertisement paging pact tests. Results are newest first, descending order. Next link changed from 'afterId' to 'beforeId'.

--- src/SEEK.AdPostingApi.SampleConsumer.Tests/GetAllAdTests.cs
+++ src/SEEK.AdPostingApi.SampleConsumer.Tests/GetAllAdTests.cs
@@ -127,83 +127,19 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
         {
             const string advertisementId1 = "fa6939b5-c91f-4f6a-9600-1ea74963fbb2";
             const string advertisementId2 = "e6e31b9c-3c2c-4b85-b17f-babbf7da972b";
-            const string nextLink = "/advertisement?after=" + advertisementId2;
+            const string advertisementJobId2 = "3";
+            const string nextLink = "/advertisement?beforeId=" + advertisementJobId2;
             OAuth2Token oAuth2Token = new OAuth2TokenBuilder().Build();
 
             RetrieveLinks();
 
             PactProvider.MockService
-                .Given("a page size of 2, and there are 2 pages worth of data")
+                .Given("A page size of 2, and there are 2 pages worth of data")
                 .UponReceiving("GET request for first page of data")
                 .With(new ProviderServiceRequest
                 {
                     Method = HttpVerb.Get,
                     Path = "/advertisement/",
-                    Headers = new Dictionary<string, string>
-                    {
-                        {"Authorization", "Bearer " + oAuth2Token.AccessToken},
-                        {"Accept", "application/hal+json"}
-                    }
-                })
-                .WillRespondWith(new ProviderServiceResponse
-                {
-                    Status = 200,
-                    Headers = new Dictionary<string, string>
-                    {
-                        {"Content-Type", "application/vnd.seek.advertisement+json; charset=utf-8"}
-                    },
-                    Body = new
-                    {
-                        _embedded = new
-                        {
-                            advertisements = new[]
-                            {
-                                new
-                                {
-                                    advertiserId = "0002",
-                                    jobTitle = "More Exciting Senior Developer role in a great CBD location. Great $$$",
-                                    jobReference = "JOB12345",
-                                    _links = new
-                                    {
-                                        self = new
-                                        {
-                                            href = "/advertisement/" + advertisementId2
-                                        }
-                                    }
-                                },
-                                new
-                                {
-                                    advertiserId = "0001",
-                                    jobTitle = "Exciting Developer role in a great CBD location. Great $$",
-                                    jobReference = "JOB1234",
-                                    _links = new
-                                    {
-                                        self = new
-                                        {
-                                            href = "/advertisement/" + advertisementId1
-                                        }
-                                    }
-                                }
-                            }
-                        },
-                        _links = new
-                        {
-                            next = new
-                            {
-                                href = nextLink
-                            }
-                        }
-                    }
-                });
-
-            PactProvider.MockService
-                .Given("a page size of 2, and there are 2 pages worth of data")
-                .UponReceiving("GET request for second page of data")
-                .With(new ProviderServiceRequest
-                {
-                    Method = HttpVerb.Get,
-                    Path = "/advertisement",
-                    Query = "after=" + advertisementId2,
                     Headers = new Dictionary<string, string>
                     {
                         {"Authorization", "Bearer " + oAuth2Token.AccessToken},
@@ -246,6 +182,71 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
                                         self = new
                                         {
                                             href = "/advertisement/" + "7bbe4318-fd3b-4d26-8384-d41489ff1dd0"
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        _links = new
+                        {
+                            next = new
+                            {
+                                href = nextLink
+                            }
+                        }
+                    }
+                });
+
+            PactProvider.MockService
+                .Given("A page size of 2, and there are 2 pages worth of data")
+                .UponReceiving("GET request for second page of data")
+                .With(new ProviderServiceRequest
+                {
+                    Method = HttpVerb.Get,
+                    Path = "/advertisement",
+                    Query = "beforeId=" + advertisementJobId2,
+                    Headers = new Dictionary<string, string>
+                    {
+                        {"Authorization", "Bearer " + oAuth2Token.AccessToken},
+                        {"Accept", "application/hal+json"}
+                    }
+                })
+                .WillRespondWith(new ProviderServiceResponse
+                {
+                    Status = 200,
+                    Headers = new Dictionary<string, string>
+                    {
+                        {"Content-Type", "application/vnd.seek.advertisement+json; charset=utf-8"}
+                    },
+                    Body = new
+                    {
+                        _embedded = new
+                        {
+                            advertisements = new[]
+                            {
+                                new
+                                {
+                                    advertiserId = "0002",
+                                    jobTitle = "More Exciting Senior Developer role in a great CBD location. Great $$$",
+                                    jobReference = "JOB12345",
+                                    _links = new
+                                    {
+                                        self = new
+                                        {
+                                            href = "/advertisement/" + advertisementId2
+                                        }
+                                    }
+                                },
+                                new
+                                {
+                                    advertiserId = "0001",
+                                    jobTitle = "Exciting Developer role in a great CBD location. Great $$",
+                                    jobReference = "JOB1234",
+                                    _links = new
+                                    {
+                                        self = new
+                                        {
+                                            href = "/advertisement/" + advertisementId1
                                         }
                                     }
                                 }
