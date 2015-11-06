8c6a367c2cc3bf0015262eec274f0ec307f16297
Amy Lin <alin@seek.com.au>
2015-11-06 12:27:57 +1100

    Added assertions for GetAll test and skip the test as known issue exists

--- src/SEEK.AdPostingApi.SampleConsumer.Tests/GetAllAdTests.cs
+++ src/SEEK.AdPostingApi.SampleConsumer.Tests/GetAllAdTests.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using PactNet.Mocks.MockHttpService.Models;
 using SEEK.AdPostingApi.Client;
+using SEEK.AdPostingApi.Client.Hal;
 using SEEK.AdPostingApi.Client.Models;
 using SEEK.AdPostingApi.Client.Resources;
 
@@ -15,6 +17,7 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
     public class GetAllAdTests : IDisposable
     {
         private readonly IOAuth2TokenClient _oauthClient;
+        private IBuilderInitializer SummaryFieldsInitializer => new SummaryFieldsInitializer();
 
         public GetAllAdTests()
         {
@@ -82,13 +85,45 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
             Assert.IsEmpty(advertisements);
         }
 
+        private string GenerateSelfLink(string advertisementId)
+        {
+            return "/advertisement/" + advertisementId;
+        }
+
+        private string GenerateViewLink(string advertisementId)
+        {
+            return "/advertisement/" + advertisementId + "/view";
+        }
+
+        private void AssertAdvertisementLinks(string advertisementId, AdvertisementSummaryResource advertisementSummaryResult)
+        {
+            StringAssert.EndsWith(GenerateSelfLink(advertisementId), advertisementSummaryResult.Uri.ToString());
+            Assert.AreEqual(GenerateSelfLink(advertisementId), advertisementSummaryResult.Links["self"].Href);
+            Assert.AreEqual(GenerateViewLink(advertisementId), advertisementSummaryResult.Links["view"].Href);
+        }
+
+        private void AssertAdvertisementSummaryAndLinks(AdvertisementModelBuilder expectedAdFirst, AdvertisementModelBuilder expectedAdSecond, AdvertisementSummaryPageResource advertisementSummaryPage, string advertisementIdFirst, string advertisementIdSecond)
+        {
+            var advertisementSummaryResult = advertisementSummaryPage.GetEnumerator().Current;
+            advertisementSummaryResult.Properties.ShouldBeEquivalentTo(expectedAdFirst);
+            AssertAdvertisementLinks(advertisementIdFirst, advertisementSummaryResult);
+            advertisementSummaryPage.GetEnumerator().MoveNext();
+            advertisementSummaryResult = advertisementSummaryPage.GetEnumerator().Current;
+            advertisementSummaryResult.Properties.ShouldBeEquivalentTo(expectedAdSecond);
+            AssertAdvertisementLinks(advertisementIdSecond, advertisementSummaryResult);
+        }
+
         [Test]
+        [Explicit("Known issue with populating AdvertisementSummary object")]
         public async Task GetAllAdvertisementsByFollowingNextLink()
         {
             const string advertisementId1 = "fa6939b5-c91f-4f6a-9600-1ea74963fbb2";
             const string advertisementId2 = "e6e31b9c-3c2c-4b85-b17f-babbf7da972b";
+            const string advertisementId3 = "7bbe4318-fd3b-4d26-8384-d41489ff1dd0";
+            const string advertisementId4 = "9141cf19-b8d7-4380-9e3f-3b5c22783bdc";
             const string advertisementJobId2 = "4";
             const string nextLink = "/advertisement?beforeId=" + advertisementJobId2;
+            const string selfLink = "/advertisement";
             OAuth2Token oAuth2Token = new OAuth2TokenBuilder().Build();
 
             PactProvider.RegisterIndexPageInteractions();
@@ -119,45 +154,25 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
                         {
                             advertisements = new[]
                             {
-                                new
-                                {
-                                    advertiserId = "0004",
-                                    jobTitle = "More Exciting Senior Developer role in a great CBD location. Great $$$",
-                                    jobReference = "JOB12347",
-                                    _links = new
-                                    {
-                                        self = new
-                                        {
-                                            href = "/advertisement/9141cf19-b8d7-4380-9e3f-3b5c22783bdc"
-                                        },
-                                        view = new
-                                        {
-                                            href = "/advertisement/9141cf19-b8d7-4380-9e3f-3b5c22783bdc/view"
-                                        }
-                                    }
-                                },
-                                new
-                                {
-                                    advertiserId = "0003",
-                                    jobTitle = "Exciting Developer role in a great CBD location. Great $$",
-                                    jobReference = "JOB1236",
-                                    _links = new
-                                    {
-                                        self = new
-                                        {
-                                            href = "/advertisement/7bbe4318-fd3b-4d26-8384-d41489ff1dd0"
-                                        },
-                                        view = new
-                                        {
-                                            href = "/advertisement/7bbe4318-fd3b-4d26-8384-d41489ff1dd0/view"
-                                        }
-                                    }
-                                }
+                                new AdvertisementContentBuilder(SummaryFieldsInitializer)
+                                    .WithAdvertiserId("0004")
+                                    .WithJobTitle("More Exciting Senior Developer role in a great CBD location. Great $$$")
+                                    .WithJobReference("JOB12347")
+                                    .WithResponseLink("self", GenerateSelfLink(advertisementId4))
+                                    .WithResponseLink("view", GenerateViewLink(advertisementId4))
+                                    .Build(),
+                                new AdvertisementContentBuilder(SummaryFieldsInitializer)
+                                    .WithAdvertiserId("0003")
+                                    .WithJobTitle("Exciting Developer role in a great CBD location. Great $$")
+                                    .WithJobReference("JOB1236")
+                                    .WithResponseLink("self", GenerateSelfLink(advertisementId3))
+                                    .WithResponseLink("view", GenerateViewLink(advertisementId3))
+                                    .Build()                         
                             }
                         },
                         _links = new
                         {
-                            self = new { href = "/advertisement" },
+                            self = new { href = selfLink },
                             next = new { href = nextLink }
                         }
                     }
@@ -190,40 +205,20 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
                         {
                             advertisements = new[]
                             {
-                                new
-                                {
-                                    advertiserId = "0002",
-                                    jobTitle = "More Exciting Senior Developer role in a great CBD location. Great $$$",
-                                    jobReference = "JOB12345",
-                                    _links = new
-                                    {
-                                        self = new
-                                        {
-                                            href = "/advertisement/" + advertisementId2
-                                        },
-                                        view = new
-                                        {
-                                            href = "/advertisement/" + advertisementId2 + "/view"
-                                        }
-                                    }
-                                },
-                                new
-                                {
-                                    advertiserId = "0001",
-                                    jobTitle = "Exciting Developer role in a great CBD location. Great $$",
-                                    jobReference = "JOB1234",
-                                    _links = new
-                                    {
-                                        self = new
-                                        {
-                                            href = "/advertisement/" + advertisementId1
-                                        },
-                                        view = new
-                                        {
-                                            href = "/advertisement/" + advertisementId1 + "/view"
-                                        }
-                                    }
-                                }
+                                new AdvertisementContentBuilder(SummaryFieldsInitializer)
+                                    .WithAdvertiserId("0002")
+                                    .WithJobTitle("More Exciting Senior Developer role in a great CBD location. Great $$$")
+                                    .WithJobReference("JOB12345")
+                                    .WithResponseLink("self", GenerateSelfLink(advertisementId2))
+                                    .WithResponseLink("view", GenerateViewLink(advertisementId2))
+                                    .Build(),
+                                new AdvertisementContentBuilder(SummaryFieldsInitializer)
+                                    .WithAdvertiserId("0001")
+                                    .WithJobTitle("Exciting Developer role in a great CBD location. Great $$")
+                                    .WithJobReference("JOB1234")
+                                    .WithResponseLink("self", GenerateSelfLink(advertisementId1))
+                                    .WithResponseLink("view", GenerateViewLink(advertisementId1))
+                                    .Build()
                             }
                         },
                         _links = new
@@ -237,8 +232,20 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
 
             var allAdvertisements = new List<AdvertisementSummaryResource>();
             AdvertisementSummaryPageResource pageResource = await client.GetAllAdvertisementsAsync();
-
+            
+            StringAssert.EndsWith(selfLink, pageResource.Uri.ToString());
             Assert.AreEqual(2, pageResource.Count());
+
+            var expectedAdvertisement4 = new AdvertisementModelBuilder(SummaryFieldsInitializer)
+                .WithAdvertiserId("0004")
+                .WithJobTitle("More Exciting Senior Developer role in a great CBD location. Great $$$")
+                .WithJobReference("JOB12347");
+            var expectedAdvertisement3 = new AdvertisementModelBuilder(SummaryFieldsInitializer)
+                .WithAdvertiserId("0003")
+                .WithJobTitle("Exciting Developer role in a great CBD location. Great $$")
+                .WithJobReference("JOB1236");
+
+            AssertAdvertisementSummaryAndLinks(expectedAdvertisement4, expectedAdvertisement3, pageResource, advertisementId4, advertisementId3);
 
             allAdvertisements.AddRange(pageResource);
 
@@ -249,6 +256,18 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
             }
 
             Assert.AreEqual(4, allAdvertisements.Count);
+            StringAssert.EndsWith(nextLink, pageResource.Uri.ToString());
+
+            var expectedAdvertisement2 = new AdvertisementModelBuilder(SummaryFieldsInitializer)
+                .WithAdvertiserId("0002")
+                .WithJobTitle("More Exciting Senior Developer role in a great CBD location. Great $$$")
+                .WithJobReference("JOB12345");
+            var expectedAdvertisement1 = new AdvertisementModelBuilder(SummaryFieldsInitializer)
+                .WithAdvertiserId("0001")
+                .WithJobTitle("Exciting Developer role in a great CBD location. Great $$")
+                .WithJobReference("JOB1234");
+
+            AssertAdvertisementSummaryAndLinks(expectedAdvertisement2, expectedAdvertisement1, pageResource, advertisementId2, advertisementId1);
 
             var actualException = Assert.Throws<NotSupportedException>(async () => await pageResource.NextPageAsync());
             var expectedException = new NotSupportedException("There are no more results");
---
+++ src/SEEK.AdPostingApi.SampleConsumer.Tests/SummaryFieldsInitializer.cs
@@ -0,0 +1,38 @@
+﻿using SEEK.AdPostingApi.Client.Models;
+
+namespace SEEK.AdPostingApi.SampleConsumer.Tests
+{
+    public class SummaryFieldsInitializer : IBuilderInitializer
+    {
+        public void Initialize(AdvertisementContentBuilder builder)
+        {
+            builder
+                .WithAdvertiserId(GetDefaultAdvertiserId())
+                .WithJobTitle(GetDefaultJobTitle())
+                .WithJobReference(GetDefaultjobReference());
+        }
+
+        public void Initialize(AdvertisementModelBuilder builder)
+        {
+            builder
+                .WithAdvertiserId(GetDefaultAdvertiserId())
+                .WithJobTitle(GetDefaultJobTitle())
+                .WithJobReference(GetDefaultjobReference());
+        }
+
+        private string GetDefaultAdvertiserId()
+        {
+            return "9012";
+        }
+
+        private string GetDefaultJobTitle()
+        {
+            return "Exciting Senior Developer role in a great CBD location. Great $$$";
+        }
+
+        private string GetDefaultjobReference()
+        {
+            return "JobReference1";
+        }
+    }
+}
