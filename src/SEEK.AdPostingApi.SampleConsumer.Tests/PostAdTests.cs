2e6d5c11e250d49785fef6fadf305c284d236d98
Grant Pedersen <gpedersen@seek.com.au>
2015-11-10 20:33:47 +1100

    add 403 pact test for create ad endpoint

--- src/SEEK.AdPostingApi.SampleConsumer.Tests/PostAdTests.cs
+++ src/SEEK.AdPostingApi.SampleConsumer.Tests/PostAdTests.cs
@@ -14,10 +14,8 @@ using SEEK.AdPostingApi.Client.Resources;
 namespace SEEK.AdPostingApi.SampleConsumer.Tests
 {
     [TestFixture]
-    public class PostAdTests : IDisposable
+    public class PostAdTests
     {
-        private readonly IOAuth2TokenClient _oauthClient;
-
         private const string AdvertisementLink = "/advertisement";
         private const string CreationIdForAdWithMinimumRequiredData = "20150914-134527-00012";
         private const string CreationIdForAdWithMaximumRequiredData = "20150914-134527-00097";
@@ -25,16 +23,6 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
         private IBuilderInitializer MinimumFieldsInitializer => new MinimumFieldsInitializer();
 
         private IBuilderInitializer AllFieldsInitializer => new AllFieldsInitializer();
-
-        public PostAdTests()
-        {
-            this._oauthClient = Mock.Of<IOAuth2TokenClient>(c => c.GetOAuth2TokenAsync() == Task.FromResult(new OAuth2TokenBuilder().Build()));
-        }
-
-        public void Dispose()
-        {
-            this._oauthClient.Dispose();
-        }
 
         [SetUp]
         public void TestInitialize()
@@ -91,9 +79,14 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
                             .Build()
                     });
 
-            var client = new AdPostingApiClient(PactProvider.MockServiceUri, _oauthClient);
             var requestModel = new AdvertisementModelBuilder(MinimumFieldsInitializer).WithRequestCreationId(CreationIdForAdWithMinimumRequiredData).Build();
-            AdvertisementResource result = await client.CreateAdvertisementAsync(requestModel);
+
+            AdvertisementResource result;
+
+            using (AdPostingApiClient client = this.GetClient(oAuth2Token))
+            {
+                result = await client.CreateAdvertisementAsync(requestModel);
+            }
 
             var expectedResult = new AdvertisementResource
             {
@@ -157,10 +150,14 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
                             .Build()
                     });
 
-            var client = new AdPostingApiClient(PactProvider.MockServiceUri, _oauthClient);
-
             var requestModel = new AdvertisementModelBuilder(AllFieldsInitializer).WithRequestCreationId(CreationIdForAdWithMaximumRequiredData).Build();
-            AdvertisementResource result = await client.CreateAdvertisementAsync(requestModel);
+
+            AdvertisementResource result;
+
+            using (AdPostingApiClient client = this.GetClient(oAuth2Token))
+            {
+                result = await client.CreateAdvertisementAsync(requestModel);
+            }
 
             var expectedResult = new AdvertisementResource
             {
@@ -241,23 +238,27 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
                         }
                     });
 
-            var client = new AdPostingApiClient(PactProvider.MockServiceUri, _oauthClient);
+            ValidationException exception;
 
-            ValidationException exception = Assert.Throws<ValidationException>(
-                async () => await client.CreateAdvertisementAsync(new AdvertisementModelBuilder(MinimumFieldsInitializer)
-                    .WithRequestCreationId("20150914-134527-00109")
-                    .WithAdvertiserId(null)
-                    .WithAdvertisementType(AdvertisementType.StandOut)
-                    .WithSalaryMinimum(0)
-                    .WithVideoUrl("htp://www.youtube.com/v/abc".PadRight(260, '!'))
-                    .WithVideoPosition(VideoPosition.Below)
-                    .WithStandoutBullets("new Uzi", "new Remington Model".PadRight(85, '!'), "new AK-47")
-                    .WithApplicationEmail("someone(at)some.domain")
-                    .WithApplicationFormUrl("htp://somecompany.domain/apply")
-                    .WithTemplateItems(
-                        new TemplateItemModel { Name = "Template Line 1", Value = "Template Value 1" },
-                        new TemplateItemModel { Name = "", Value = "value2".PadRight(3010, '!') })
-                    .Build()));
+            using (AdPostingApiClient client = this.GetClient(oAuth2Token))
+            {
+                exception = Assert.Throws<ValidationException>(
+                    async () =>
+                        await client.CreateAdvertisementAsync(new AdvertisementModelBuilder(MinimumFieldsInitializer)
+                            .WithRequestCreationId("20150914-134527-00109")
+                            .WithAdvertiserId(null)
+                            .WithAdvertisementType(AdvertisementType.StandOut)
+                            .WithSalaryMinimum(0)
+                            .WithVideoUrl("htp://www.youtube.com/v/abc".PadRight(260, '!'))
+                            .WithVideoPosition(VideoPosition.Below)
+                            .WithStandoutBullets("new Uzi", "new Remington Model".PadRight(85, '!'), "new AK-47")
+                            .WithApplicationEmail("someone(at)some.domain")
+                            .WithApplicationFormUrl("htp://somecompany.domain/apply")
+                            .WithTemplateItems(
+                                new TemplateItemModel { Name = "Template Line 1", Value = "Template Value 1" },
+                                new TemplateItemModel { Name = "", Value = "value2".PadRight(3010, '!') })
+                            .Build()));
+            }
 
             var expectedException = new ValidationException(
                 HttpMethod.Post,
@@ -321,10 +322,13 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
                         }
                     });
 
-            var client = new AdPostingApiClient(PactProvider.MockServiceUri, _oauthClient);
+            ValidationException exception;
 
-            ValidationException exception = Assert.Throws<ValidationException>(
-                async () => await client.CreateAdvertisementAsync(new AdvertisementModelBuilder(MinimumFieldsInitializer).Build()));
+            using (AdPostingApiClient client = this.GetClient(oAuth2Token))
+            {
+                exception = Assert.Throws<ValidationException>(
+                    async () => await client.CreateAdvertisementAsync(new AdvertisementModelBuilder(MinimumFieldsInitializer).Build()));
+            }
 
             exception.ShouldBeEquivalentToException(
                 new ValidationException(
@@ -369,14 +373,75 @@ namespace SEEK.AdPostingApi.SampleConsumer.Tests
                         Headers = new Dictionary<string, string> { { "Location", location } }
                     });
 
-            var client = new AdPostingApiClient(PactProvider.MockServiceUri, _oauthClient);
+            AdvertisementAlreadyExistsException actualException;
 
-            AdvertisementAlreadyExistsException actualException = Assert.Throws<AdvertisementAlreadyExistsException>(
-                async () => await client.CreateAdvertisementAsync(new AdvertisementModelBuilder(MinimumFieldsInitializer).WithRequestCreationId(creationId).Build()));
+            using (AdPostingApiClient client = this.GetClient(oAuth2Token))
+            {
+                actualException = Assert.Throws<AdvertisementAlreadyExistsException>(
+                    async () => await client.CreateAdvertisementAsync(new AdvertisementModelBuilder(MinimumFieldsInitializer).WithRequestCreationId(creationId).Build()));
+            }
 
             var expectedException = new AdvertisementAlreadyExistsException(new Uri(location));
 
             actualException.ShouldBeEquivalentToException(expectedException);
+        }
+
+        [Test]
+        public void PostAdNotPermitted()
+        {
+            const string advertisementId = "75b2b1fc-9050-4f45-a632-ec6b7ac2bb4a";
+            OAuth2Token oAuth2Token = new OAuth2TokenBuilder().Build();
+            var link = $"{AdvertisementLink}/{advertisementId}";
+
+            PactProvider.RegisterIndexPageInteractions(oAuth2Token);
+
+            PactProvider.MockService
+                .UponReceiving("an unauthorised request to create a job ad")
+                .With(
+                    new ProviderServiceRequest
+                    {
+                        Method = HttpVerb.Post,
+                        Path = AdvertisementLink,
+                        Headers = new Dictionary<string, string>
+                        {
+                            { "Authorization", "Bearer " + oAuth2Token.AccessToken },
+                            { "Content-Type", "application/vnd.seek.advertisement+json; charset=utf-8" }
+                        },
+                        Body = new AdvertisementContentBuilder(MinimumFieldsInitializer)
+                            .WithRequestCreationId(CreationIdForAdWithMinimumRequiredData)
+                            .Build()
+                    }
+                )
+                .WillRespondWith(
+                    new ProviderServiceResponse
+                    {
+                        Status = 403,
+                        Headers = new Dictionary<string, string>
+                        {
+                            { "Content-Type", "application/json; charset=utf-8" }
+                        },
+                        Body = new { message = "Operation not permitted on advertisement with advertiser id: '9012'" }
+                    });
+
+            var requestModel = new AdvertisementModelBuilder(MinimumFieldsInitializer).WithRequestCreationId(CreationIdForAdWithMinimumRequiredData).Build();
+
+            UnauthorizedException actualException;
+
+            using (AdPostingApiClient client = this.GetClient(oAuth2Token))
+            {
+                actualException = Assert.Throws<UnauthorizedException>(
+                    async () => await client.CreateAdvertisementAsync(requestModel));
+            }
+
+            actualException.ShouldBeEquivalentToException(
+                new UnauthorizedException("Operation not permitted on advertisement with advertiser id: '9012'"));
+        }
+
+        private AdPostingApiClient GetClient(OAuth2Token token)
+        {
+            var oAuthClient = Mock.Of<IOAuth2TokenClient>(c => c.GetOAuth2TokenAsync() == Task.FromResult(token));
+
+            return new AdPostingApiClient(PactProvider.MockServiceUri, oAuthClient);
         }
     }
 }
