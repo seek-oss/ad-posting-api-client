b078890f1a3276d8b697a11a45a4be508e7ad48e
Amy Lin <alin@seek.com.au>
2016-04-08 10:51:12 +1000

    Added pact test cases for get all by advertiser

--- src/SEEK.AdPostingApi.Client.Tests/GetAllAdTests.cs
+++ src/SEEK.AdPostingApi.Client.Tests/GetAllAdTests.cs
@@ -316,22 +316,22 @@ namespace SEEK.AdPostingApi.Client.Tests
         }
 
         [Fact]
-        public async Task GetAllAdvertisementsByAdvertiser()
+        public async Task GetAllAdvertisementsByAdvertiserFirstPage()
         {
             const string advertiser = "456";
-            const string advertisementId1 = "7bbe4318-fd3b-4d26-8384-d41489ff1dd0";
-            const string advertisementId2 = "9141cf19-b8d7-4380-9e3f-3b5c22783bdc";
-            //const string advertisementJobId2 = "2";
-            //const string nextLink = "/advertisement?beforeId=" + advertisementJobId2;
+            const string advertisementId3 = "7bbe4318-fd3b-4d26-8384-d41489ff1dd0";
+            const string advertisementId4 = "9141cf19-b8d7-4380-9e3f-3b5c22783bdc";
+            const string advertisementJobId2 = "6";
             const string queryString = "advertiserId=" + advertiser;
             const string selfLink = "/advertisement?" + queryString;
+            const string nextLink = "/advertisement?" + queryString + "&beforeId=" + advertisementJobId2;
             OAuth2Token oAuth2Token = new OAuth2TokenBuilder().Build();
 
             this.Fixture.RegisterIndexPageInteractions(oAuth2Token);
 
             this.Fixture.AdPostingApiService
-                .Given("A page size of 2, and there are 2 advertisements belong to a advertiser")
-                .UponReceiving("GET request for advertisements belong to the advertiser")
+                .Given("A page size of 2, and there are 4 advertisements belong to a advertiser")
+                .UponReceiving("GET request for the first page of advertisements belong to the advertiser")
                 .With(new ProviderServiceRequest
                 {
                     Method = HttpVerb.Get,
@@ -358,24 +358,24 @@ namespace SEEK.AdPostingApi.Client.Tests
                             {
                                 new AdvertisementContentBuilder(SummaryFieldsInitializer)
                                     .WithAdvertiserId(advertiser)
-                                    .WithJobTitle("More Exciting Senior Developer role in a great CBD location. Great $$$")
-                                    .WithJobReference("JOB12347")
-                                    .WithResponseLink("self", GenerateSelfLink(advertisementId2))
-                                    .WithResponseLink("view", GenerateViewLink(advertisementId2))
+                                    .WithJobTitle("More Exciting Senior Tester role in a great CBD location. Great $$$")
+                                    .WithJobReference("JOB4444")
+                                    .WithResponseLink("self", GenerateSelfLink(advertisementId4))
+                                    .WithResponseLink("view", GenerateViewLink(advertisementId4))
                                     .Build(),
                                 new AdvertisementContentBuilder(SummaryFieldsInitializer)
                                     .WithAdvertiserId(advertiser)
-                                    .WithJobTitle("Exciting Developer role in a great CBD location. Great $$")
-                                    .WithJobReference("JOB1236")
-                                    .WithResponseLink("self", GenerateSelfLink(advertisementId1))
-                                    .WithResponseLink("view", GenerateViewLink(advertisementId1))
+                                    .WithJobTitle("More Exciting Senior Developer role in a great CBD location. Great $$$")
+                                    .WithJobReference("JOB3333")
+                                    .WithResponseLink("self", GenerateSelfLink(advertisementId3))
+                                    .WithResponseLink("view", GenerateViewLink(advertisementId3))
                                     .Build()
                             }
                         },
                         _links = new
                         {
-                            self = new { href = selfLink }//,
-                            //next = new { href = nextLink }
+                            self = new { href = selfLink },
+                            next = new { href = nextLink }
                         }
                     }
                 });
@@ -394,34 +394,264 @@ namespace SEEK.AdPostingApi.Client.Tests
                     new AdvertisementSummaryResource
                     {
                         AdvertiserId = advertiser,
-                        JobReference = "JOB12347",
-                        JobTitle = "More Exciting Senior Developer role in a great CBD location. Great $$$",
+                        JobReference = "JOB4444",
+                        JobTitle = "More Exciting Senior Tester role in a great CBD location. Great $$$",
                         Links = new Links(this.Fixture.AdPostingApiServiceBaseUri)
                         {
-                            { "self", new Link { Href = $"/advertisement/{advertisementId2}" } },
-                            { "view", new Link { Href = $"/advertisement/{advertisementId2}/view" } }
+                            { "self", new Link { Href = $"/advertisement/{advertisementId4}" } },
+                            { "view", new Link { Href = $"/advertisement/{advertisementId4}/view" } }
                         }
                     },
                     new AdvertisementSummaryResource
                     {
                         AdvertiserId = advertiser,
-                        JobReference = "JOB1236",
-                        JobTitle = "Exciting Developer role in a great CBD location. Great $$",
+                        JobReference = "JOB3333",
+                        JobTitle = "More Exciting Senior Developer role in a great CBD location. Great $$$",
                         Links = new Links(this.Fixture.AdPostingApiServiceBaseUri)
                         {
-                            { "self", new Link { Href = $"/advertisement/{advertisementId1}" } },
-                            { "view", new Link { Href = $"/advertisement/{advertisementId1}/view" } }
+                            { "self", new Link { Href = $"/advertisement/{advertisementId3}" } },
+                            { "view", new Link { Href = $"/advertisement/{advertisementId3}/view" } }
                         }
                     }
                 },
                 Links = new Links(this.Fixture.AdPostingApiServiceBaseUri)
                 {
-                    { "self", new Link { Href = selfLink } }//,
-                    //{ "next", new Link { Href = "/advertisement?beforeId=4"} }
+                    { "self", new Link { Href = selfLink } },
+                    { "next", new Link { Href = nextLink } }
                 }
             };
 
             pageResource.ShouldBeEquivalentTo(expectedPageResource);
+        }
+
+        [Fact]
+        public async Task GetAllAdvertisementsByAdvertiserNextPage()
+        {
+            const string advertiserId = "456";
+            const string advertisementId1 = "f7302df2-704b-407c-a42a-62ff822b5461";
+            const string advertisementId2 = "3b138935-f65b-4ec7-91d8-fc250757b53d";
+            const string advertisementJobId2 = "6";
+            const string queryString = "advertiserId=" + advertiserId + "&beforeId=" + advertisementJobId2;
+            const string selfLink = "/advertisement?" + queryString;
+            OAuth2Token oAuth2Token = new OAuth2TokenBuilder().Build();
+
+            this.Fixture.AdPostingApiService
+                .Given("A page size of 2, and there are 4 advertisements belong to a advertiser")
+                .UponReceiving("GET request for the second page of advertisements belong to the advertiser")
+                .With(new ProviderServiceRequest
+                {
+                    Method = HttpVerb.Get,
+                    Path = "/advertisement",
+                    Query = queryString,
+                    Headers = new Dictionary<string, string>
+                    {
+                        {"Authorization", "Bearer " + oAuth2Token.AccessToken},
+                        {"Accept", "application/hal+json"}
+                    }
+                })
+                .WillRespondWith(new ProviderServiceResponse
+                {
+                    Status = 200,
+                    Headers = new Dictionary<string, string>
+                    {
+                        {"Content-Type", "application/vnd.seek.advertisement-list+json; version=1; charset=utf-8"}
+                    },
+                    Body = new
+                    {
+                        _embedded = new
+                        {
+                            advertisements = new[]
+                            {
+                                new AdvertisementContentBuilder(SummaryFieldsInitializer)
+                                    .WithAdvertiserId(advertiserId)
+                                    .WithJobTitle(
+                                        "Exciting tester role in a great CBD location. Great $$")
+                                    .WithJobReference("JOB2222")
+                                    .WithResponseLink("self", GenerateSelfLink(advertisementId2))
+                                    .WithResponseLink("view", GenerateViewLink(advertisementId2))
+                                    .Build(),
+                                new AdvertisementContentBuilder(SummaryFieldsInitializer)
+                                    .WithAdvertiserId(advertiserId)
+                                    .WithJobTitle("Exciting Developer role in a great CBD location. Great $$")
+                                    .WithJobReference("JOB1111")
+                                    .WithResponseLink("self", GenerateSelfLink(advertisementId1))
+                                    .WithResponseLink("view", GenerateViewLink(advertisementId1))
+                                    .Build()
+                            }
+                        },
+                        _links = new
+                        {
+                            self = new { href = selfLink }
+                        }
+                    }
+                });
+
+            AdvertisementSummaryPageResource pageResource = new AdvertisementSummaryPageResource
+            {
+                Links = new Links(this.Fixture.AdPostingApiServiceBaseUri)
+                {
+                    {"self", new Link {Href = "/advertisement"}},
+                    {"next", new Link {Href = $"/advertisement?advertiserId={advertiserId}&beforeId={advertisementJobId2}"}}
+                }
+            };
+
+            var oAuthClient = Mock.Of<IOAuth2TokenClient>(c => c.GetOAuth2TokenAsync() == Task.FromResult(oAuth2Token));
+            AdvertisementSummaryPageResource nextPageResource;
+
+            using (var client = new Client.Hal.Client(new HttpClient(new OAuthMessageHandler(oAuthClient))))
+            {
+                pageResource.Initialise(client);
+
+                nextPageResource = await pageResource.NextPageAsync();
+            }
+
+            AdvertisementSummaryPageResource expectedNextPageResource = new AdvertisementSummaryPageResource
+            {
+                AdvertisementSummaries = new List<AdvertisementSummaryResource>
+                {
+                    new AdvertisementSummaryResource
+                    {
+                        AdvertiserId = advertiserId,
+                        JobReference = "JOB2222",
+                        JobTitle = "Exciting tester role in a great CBD location. Great $$",
+                        Links = new Links(this.Fixture.AdPostingApiServiceBaseUri)
+                        {
+                            {"self", new Link {Href = $"/advertisement/{advertisementId2}"}},
+                            {"view", new Link {Href = $"/advertisement/{advertisementId2}/view"}}
+                        }
+                    },
+                    new AdvertisementSummaryResource
+                    {
+                        AdvertiserId = advertiserId,
+                        JobReference = "JOB1111",
+                        JobTitle = "Exciting Developer role in a great CBD location. Great $$",
+                        Links = new Links(this.Fixture.AdPostingApiServiceBaseUri)
+                        {
+                            {"self", new Link {Href = $"/advertisement/{advertisementId1}"}},
+                            {"view", new Link {Href = $"/advertisement/{advertisementId1}/view"}}
+                        }
+                    }
+                },
+                Links = new Links(this.Fixture.AdPostingApiServiceBaseUri)
+                {
+                    {"self", new Link {Href = selfLink}}
+                }
+            };
+
+            nextPageResource.ShouldBeEquivalentTo(expectedNextPageResource);
+        }
+
+        [Fact]
+        public async Task GetAllAdvertisementByAdvertiserWithNonExistentAdvertiserId()
+        {
+            string advertiser = Guid.NewGuid().ToString();
+            string queryString = "advertiserId=" + advertiser;
+            OAuth2Token oAuth2Token = new OAuth2TokenBuilder().Build();
+
+            this.Fixture.RegisterIndexPageInteractions(oAuth2Token);
+
+            this.Fixture.AdPostingApiService
+                .UponReceiving("GET request to retrieve all advertisements for an advertiser not exists")
+                .With(new ProviderServiceRequest
+                {
+                    Method = HttpVerb.Get,
+                    Path = "/advertisement",
+                    Query = queryString,
+                    Headers = new Dictionary<string, string>
+                    {
+                        {"Authorization", "Bearer " + oAuth2Token.AccessToken},
+                        {"Accept", "application/hal+json"}
+                    }
+                })
+                .WillRespondWith(new ProviderServiceResponse
+                {
+                    Status = 403,
+                    Headers = new Dictionary<string, string>
+                        {
+                            { "Content-Type", "application/vnd.seek.advertisement-error+json; version=1; charset=utf-8" }
+                        },
+                    Body = new
+                    {
+                        message = "Forbidden",
+                        errors = new[]
+                            {
+                                new { code = "InvalidValue" }
+                            }
+                    }
+                });
+
+            UnauthorizedException actualException;
+            using (AdPostingApiClient client = this.Fixture.GetClient(oAuth2Token))
+            {
+                actualException = await Assert.ThrowsAsync<UnauthorizedException>(
+                    async () => await client.GetAllAdvertisementsAsync(advertiser));
+            }
+
+            actualException.ShouldBeEquivalentToException(
+                new UnauthorizedException(
+                    new ForbiddenMessage
+                    {
+                        Message = "Forbidden",
+                        Errors = new[] { new ForbiddenMessageData { Code = "InvalidValue" } }
+                    }
+                    ));
+        }
+
+        [Fact]
+        public async Task GetAllAdvertisementsByAdvertiserReturnsRelationshipError()
+        {
+            var advertiserId = "874392";
+            string queryString = "advertiserId=" + advertiserId;
+            OAuth2Token oAuth2Token = new OAuth2TokenBuilder().Build();
+
+            this.Fixture.RegisterIndexPageInteractions(oAuth2Token);
+
+            this.Fixture.AdPostingApiService
+                .Given("The adveritser account doesn't have relationship to any uploader")
+                .UponReceiving("GET request to retrieve all advertisements for the advertiser not related to uploader")
+                .With(new ProviderServiceRequest
+                {
+                    Method = HttpVerb.Get,
+                    Path = "/advertisement",
+                    Query = queryString,
+                    Headers = new Dictionary<string, string>
+                    {
+                        {"Authorization", "Bearer " + oAuth2Token.AccessToken},
+                        {"Accept", "application/hal+json"}
+                    }
+                })
+                .WillRespondWith(new ProviderServiceResponse
+                {
+                    Status = 403,
+                    Headers = new Dictionary<string, string>
+                        {
+                            { "Content-Type", "application/vnd.seek.advertisement-error+json; version=1; charset=utf-8" }
+                        },
+                    Body = new
+                    {
+                        message = "Forbidden",
+                        errors = new[]
+                            {
+                                new { code = "RelationshipError" }
+                            }
+                    }
+                });
+
+            UnauthorizedException actualException;
+            using (AdPostingApiClient client = this.Fixture.GetClient(oAuth2Token))
+            {
+                actualException = await Assert.ThrowsAsync<UnauthorizedException>(
+                    async () => await client.GetAllAdvertisementsAsync(advertiserId));
+            }
+
+            actualException.ShouldBeEquivalentToException(
+                new UnauthorizedException(
+                    new ForbiddenMessage
+                    {
+                        Message = "Forbidden",
+                        Errors = new[] { new ForbiddenMessageData { Code = "RelationshipError" } }
+                    }
+                    ));
         }
 
         private string GenerateSelfLink(string advertisementId)
