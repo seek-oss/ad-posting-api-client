cdfbaa9da194c590742fbc62216ccde184094fb5
Amy Lin <alin@seek.com.au>
2016-07-12 08:48:04 +1000

    cosmetic

--- src/SEEK.AdPostingApi.Client.Tests/ExpireAdTests.cs
+++ src/SEEK.AdPostingApi.Client.Tests/ExpireAdTests.cs
@@ -1,15 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Dynamic;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
-using Marvin.JsonPatch.Operations;
-using PactNet.Mocks.MockHttpService;
 using PactNet.Mocks.MockHttpService.Models;
 using SEEK.AdPostingApi.Client.Models;
 using SEEK.AdPostingApi.Client.Resources;
