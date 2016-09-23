2dfb9258adffe6e655078a5c43b5084616aab76f
Faiz Hasan <fhasan@seek.com.au>
2016-09-23 14:16:12 +1000

    add missing initialiser for Errors property in UnauthorizedException

--- src/SEEK.AdPostingApi.Client/UnauthorizedException.cs
+++ src/SEEK.AdPostingApi.Client/UnauthorizedException.cs
@@ -9,6 +9,7 @@ namespace SEEK.AdPostingApi.Client
     {
         public UnauthorizedException(string requestId, int httpStatusCode, string message) : base(requestId, httpStatusCode, message)
         {
+            this.Errors = new AdvertisementError[0];
         }
 
         public UnauthorizedException(string requestId, int httpStatusCode, AdvertisementErrorResponse errorResponse) : base(requestId, httpStatusCode, errorResponse?.Message)
