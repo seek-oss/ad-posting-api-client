beb8c357c2cc5b5306b2db2741f44848dfa2c8ba
Stephanie Jeffreys <sjeffreys@seek.com.au>
2016-12-15 16:01:10 +1100

    Added the failed and the pending status back and made them obsolete.

--- src/SEEK.AdPostingApi.Client/Models/ProcessingStatus.cs
+++ src/SEEK.AdPostingApi.Client/Models/ProcessingStatus.cs
@@ -1,8 +1,18 @@
-﻿namespace SEEK.AdPostingApi.Client.Models
+﻿using System;
+
+namespace SEEK.AdPostingApi.Client.Models
 {
+    [Obsolete("The processing status will always be completed. All validation is done upfront and the advertisement will not fail once successfully submitted.")]
     public enum ProcessingStatus
     {
         Unknown,
+
+        [Obsolete("The processing status will always be completed. All validation is done upfront and the advertisement will not fail once successfully submitted.")]
+        Failed,
+
+        [Obsolete("The processing status will always be completed. All validation is done upfront and the advertisement will not fail once successfully submitted.")]
+        Pending,
+
         Completed
     }
 }
