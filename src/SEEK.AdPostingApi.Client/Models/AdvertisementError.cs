43a3e4f5948d81fc899696017e561b1995575d43
RJ Lohan <rlohan@seek.com.au>
2017-06-29 13:40:46 +1000

    Re-introduced obsoleted AdvertisementError, per PR review comments.

---
+++ src/SEEK.AdPostingApi.Client/Models/AdvertisementError.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SEEK.AdPostingApi.Client.Models
+{
+    [Serializable]
+    [Obsolete("Use general type SEEK.AdPostingApi.Client.Models.Error")]
+    public class AdvertisementError
+    {
+        public string Field { get; set; }
+
+        public string Code { get; set; }
+
+        public string Message { get; set; }
+
+        public static implicit operator AdvertisementError(Error error)
+        {
+            return new AdvertisementError
+            {
+                Code = error.Code,
+                Field = error.Field,
+                Message = error.Message
+            };
+        }
+    }
+}
