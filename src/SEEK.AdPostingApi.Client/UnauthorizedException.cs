09c67db2ed17163438f5d67f81c552d7628b6289
Zachary Sims <zsims@seek.com.au>
2018-02-02 08:55:24 +1100

    Targets .NET Standard 1.5
    .NET Standard 2.0 requires no code changes, but only allows us to target .NET Core 2+ (which is fairly new).
    Per the Microsoft recommendation we should target the lowest possible version: https://docs.microsoft.com/en-us/dotnet/standard/net-standard
    
    As a result this required some minor modifications to the code, the only client-visible change is
    exceptions are no longer serializable which would only impact clients that pass exceptions between AppDomains (an edge case at best)

--- src/SEEK.AdPostingApi.Client/UnauthorizedException.cs
+++ src/SEEK.AdPostingApi.Client/UnauthorizedException.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Runtime.Serialization;
-using SEEK.AdPostingApi.Client.Models;
+﻿using SEEK.AdPostingApi.Client.Models;
 
 namespace SEEK.AdPostingApi.Client
 {
-    [Serializable]
     public class UnauthorizedException : RequestException
     {
         public UnauthorizedException(string requestId, int httpStatusCode, string message) : base(requestId, httpStatusCode, message)
@@ -17,18 +14,6 @@ namespace SEEK.AdPostingApi.Client
             this.Errors = errorResponse?.Errors ?? new AdvertisementError[0];
         }
 
-        protected UnauthorizedException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-            this.Errors = (AdvertisementError[])info.GetValue(nameof(this.Errors), typeof(AdvertisementError[]));
-        }
-
         public AdvertisementError[] Errors { get; }
-
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            info.AddValue(nameof(this.Errors), this.Errors);
-
-            base.GetObjectData(info, context);
-        }
     }
 }
--- src/SEEK.AdPostingApi.Client/ValidationException.cs
+++ src/SEEK.AdPostingApi.Client/ValidationException.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Net.Http;
-using System.Runtime.Serialization;
 using SEEK.AdPostingApi.Client.Models;
 
 namespace SEEK.AdPostingApi.Client
 {
-    [Serializable]
     public class ValidationException : RequestException
     {
         public ValidationException(string requestId, HttpMethod method, AdvertisementErrorResponse errorResponse)
@@ -14,18 +12,6 @@ namespace SEEK.AdPostingApi.Client
             this.Errors = errorResponse?.Errors ?? new AdvertisementError[0];
         }
 
-        protected ValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-            this.Errors = (AdvertisementError[])info.GetValue(nameof(this.Errors), typeof(AdvertisementError[]));
-        }
-
         public AdvertisementError[] Errors { get; }
-
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            info.AddValue(nameof(this.Errors), this.Errors);
-
-            base.GetObjectData(info, context);
-        }
     }
 }
