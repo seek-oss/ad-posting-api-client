f237a755f56ab5f4ffa0cb31d2ac30228aa8adcb
Zachary Sims <zsims@seek.com.au>
2018-02-01 15:00:51 +1100

    Restores the initial version number of the Ad Posting API Client

--- src/SEEK.AdPostingApi.Client/Properties/AssemblyInfoGenerated.cs
+++ src/SEEK.AdPostingApi.Client/Properties/AssemblyInfoGenerated.cs
@@ -5,7 +5,7 @@
 //------------------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-task-upgrade-project-format")]
+[assembly: AssemblyVersion("0.15.630.1108")]
+[assembly: AssemblyFileVersion("0.15.630.1108")]
+[assembly: AssemblyInformationalVersion("0.15.630.1108-commitHashPlaceholder-commitBranchPlaceholder")]
 
